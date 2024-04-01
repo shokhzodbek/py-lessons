fc4f4a68412057fe9a59a26053951a62cce99bfb
shokhzodbek <66237287+shokhzodbek@users.noreply.github.com>
2024-04-01 08:46:36 +0500

    added files

---
+++ main.py
@@ -0,0 +1,75 @@
+# a = int(input("Son kirit "))
+# if a>0:
+#     a+=1
+# else:
+#     a-=2
+
+# ielts = int(input("IELTS ballingni kirit "))
+# if ielts==5:
+#     print("Uz")
+# elif ielts==6:
+#     print("Kar")
+# elif ielts==7:
+#     print("Ka")
+# elif ielts==8:
+#     print("Uk")
+# elif ielts==9:
+#     print("USA")
+# else:
+#     print("Bunaqa bal bilan Marsda oqiysan")
+# if ielts==5:
+#     print("Uzbekistan")
+
+# if ielts==6:
+#     print('Kariya')
+
+# if ielts == 7:
+#     print("Kanada")
+
+# num1 = int(input("1-sonni kirit "))
+# ar_amal = input("Arifmitik amalni kirit ")
+# num2 = int(input("2-sonni kirit "))
+
+# if ar_amal=='+':
+#     print(num1+num2)
+# elif ar_amal=='-':
+#     print(num1-num2)
+# elif ar_amal=="*":
+#     print(num1*num2)
+# elif ar_amal=="/":
+#     print(num1/num2)
+# else:
+#     print("Bunaqa amal yuq")
+
+# a = int(input("a ni kirit "))
+# if a == 5:
+#     print("a = 5")
+# if a!=5:
+#     print("a teng emas 5 ga ")
+
+# name = input("Ism kirit ")
+# if name!='ali':
+#     print("Xush kelibsan")
+# else:
+#     print("Mumkinmas senga ")
+
+# age = int(input("Yoshingni kirit "))
+# if age < 7:
+#     print("Tiken senga")
+# elif age >=7 and age < 60:
+#     print("2000 sum")
+# elif age >= 60:
+#     print("1000 sum")
+    
+# name = input("Ismingni kirit ")
+
+# if name=='ali':
+#     age = int(input("Yoshingni kirit "))
+#     if age > 25:
+#         print("Yoshing katta 25")
+#     familya = input("Familyani kirit  ")
+#     print(f"ism {name} familya {familya} yosh {age}")
+oy  = int(input("Oyni kirit "))
+if oy==1 or oy==2 or oy==12:
+    print("Qish")
+
